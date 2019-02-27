4ddcb60c6ec9e674537feb5a0dc5a6344a4c1292
Oleg Serikov <srkvoa@gmail.com>
2019-02-27 11:59:51 +0300

    deleted everything unnecessary in the task

---
+++ utils/__init__.py
@@ -0,0 +1,303 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import io
+import sys
+import numpy as np
+import torch
+
+
+# load Faiss if available (dramatically accelerates the nearest neighbor search)
+try:
+    import faiss
+
+    FAISS_AVAILABLE = True
+    if not hasattr(faiss, 'StandardGpuResources'):
+        sys.stderr.write("Impossible to import Faiss-GPU. "
+                         "Switching to FAISS-CPU, "
+                         "this will be slower.\n\n")
+
+except ImportError:
+    sys.stderr.write("Impossible to import Faiss library!! "
+                     "Switching to standard nearest neighbors search implementation, "
+                     "this will be significantly slower.\n\n")
+    FAISS_AVAILABLE = False
+
+
+class Dictionary(object):
+
+    def __init__(self, id2word, word2id, lang):
+        assert len(id2word) == len(word2id)
+        self.id2word = id2word
+        self.word2id = word2id
+        self.lang = lang
+        self.check_valid()
+
+    def __len__(self):
+        """
+        Returns the number of words in the dictionary.
+        """
+        return len(self.id2word)
+
+    def __getitem__(self, i):
+        """
+        Returns the word of the specified index.
+        """
+        return self.id2word[i]
+
+    def __contains__(self, w):
+        """
+        Returns whether a word is in the dictionary.
+        """
+        return w in self.word2id
+
+    def __eq__(self, y):
+        """
+        Compare the dictionary with another one.
+        """
+        self.check_valid()
+        y.check_valid()
+        if len(self.id2word) != len(y):
+            return False
+        return self.lang == y.lang and all(self.id2word[i] == y[i] for i in range(len(y)))
+
+    def check_valid(self):
+        """
+        Check that the dictionary is valid.
+        """
+        assert len(self.id2word) == len(self.word2id)
+        for i in range(len(self.id2word)):
+            assert self.word2id[self.id2word[i]] == i
+
+    def index(self, word):
+        """
+        Returns the index of the specified word.
+        """
+        return self.word2id[word]
+
+    def prune(self, max_vocab):
+        """
+        Limit the vocabulary size.
+        """
+        assert max_vocab >= 1
+        self.id2word = {k: v for k, v in self.id2word.items() if k < max_vocab}
+        self.word2id = {v: k for k, v in self.id2word.items()}
+        self.check_valid()
+
+
+def get_nn_avg_dist(emb, query, knn):
+    """
+    Compute the average distance of the `knn` nearest neighbors
+    for a given set of embeddings and queries.
+    Use Faiss if available.
+    """
+    if FAISS_AVAILABLE:
+        emb = emb.cpu().numpy()
+        query = query.cpu().numpy()
+        if hasattr(faiss, 'StandardGpuResources'):
+            # gpu mode
+            res = faiss.StandardGpuResources()
+            config = faiss.GpuIndexFlatConfig()
+            config.device = 0
+            index = faiss.GpuIndexFlatIP(res, emb.shape[1], config)
+        else:
+            # cpu mode
+            index = faiss.IndexFlatIP(emb.shape[1])
+        index.add(emb)
+        distances, _ = index.search(query, knn)
+        return distances.mean(1)
+    else:
+        bs = 1024
+        all_distances = []
+        emb = emb.transpose(0, 1).contiguous()
+        for i in range(0, query.shape[0], bs):
+            distances = query[i:i + bs].mm(emb)
+            best_distances, _ = distances.topk(knn, dim=1, largest=True, sorted=True)
+            all_distances.append(best_distances.mean(1).cpu())
+        all_distances = torch.cat(all_distances)
+        return all_distances.numpy()
+
+
+def read_txt_embeddings(lang, emb_path, emb_dim, max_vocab, full_vocab):
+    """
+    Reload pretrained embeddings from a text file.
+    """
+    word2id = {}
+    vectors = []
+
+    # load pretrained embeddings
+    #     lang = lang if source else params.tgt_lang
+    #     emb_path = params.src_emb if source else params.tgt_emb
+    _emb_dim_file = emb_dim
+    with io.open(emb_path, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                split = line.split()
+                assert len(split) == 2
+                assert _emb_dim_file == int(split[1])
+            else:
+                word, vect = line.rstrip().split(' ', 1)
+                if not full_vocab:
+                    word = word.lower()
+                vect = np.fromstring(vect, sep=' ')
+                if np.linalg.norm(vect) == 0:  # avoid to have null embeddings
+                    vect[0] = 0.01
+                if word in word2id:
+                    if full_vocab:
+                        pass
+                #                         logger.warning("Word '%s' found twice in %s embedding file"
+                #                                        % (word, 'source' if source else 'target'))
+                else:
+                    if not vect.shape == (_emb_dim_file,):
+                        # logger.warning("Invalid dimension (%i) for %s word '%s' in line %i."
+                        #     % (vect.shape[0], 'source' if source else 'target', word, i))
+                        continue
+                    assert vect.shape == (_emb_dim_file,), i
+                    word2id[word] = len(word2id)
+                    vectors.append(vect[None])
+            if max_vocab > 0 and len(word2id) >= max_vocab and not full_vocab:
+                break
+
+    assert len(word2id) == len(vectors)
+    print("Loaded %i pre-trained word embeddings." % len(vectors))
+
+    # compute new vocabulary / embeddings
+    id2word = {v: k for k, v in word2id.items()}
+    dico = Dictionary(id2word, word2id, lang)
+    embeddings = np.concatenate(vectors, 0)
+    embeddings = torch.from_numpy(embeddings).float()
+    #     embeddings = embeddings.cuda() if (params.cuda and not full_vocab) else embeddings
+
+    assert embeddings.size() == (len(dico), emb_dim)
+    return dico, embeddings
+
+
+def get_candidates(emb1, emb2, dico_max_rank, dico_method, dico_min_size, dico_max_size, dico_threshold):
+    """
+    Get best translation pairs candidates.
+    """
+    bs = 128
+
+    all_scores = []
+    all_targets = []
+
+    # number of source words to consider
+    n_src = emb1.size(0)
+    if dico_max_rank > 0 and not dico_method.startswith('invsm_beta_'):
+        n_src = min(dico_max_rank, n_src)
+
+    # nearest neighbors
+    if dico_method == 'nn':
+
+        # for every source word
+        for i in range(0, n_src, bs):
+
+            # compute target words scores
+            scores = emb2.mm(emb1[i:min(n_src, i + bs)].transpose(0, 1)).transpose(0, 1)
+            best_scores, best_targets = scores.topk(2, dim=1, largest=True, sorted=True)
+
+            # update scores / potential targets
+            all_scores.append(best_scores.cpu())
+            all_targets.append(best_targets.cpu())
+
+        all_scores = torch.cat(all_scores, 0)
+        all_targets = torch.cat(all_targets, 0)
+
+    # inverted softmax
+    elif dico_method.startswith('invsm_beta_'):
+
+        beta = float(dico_method[len('invsm_beta_'):])
+
+        # for every target word
+        for i in range(0, emb2.size(0), bs):
+
+            # compute source words scores
+            scores = emb1.mm(emb2[i:i + bs].transpose(0, 1))
+            scores.mul_(beta).exp_()
+            scores.div_(scores.sum(0, keepdim=True).expand_as(scores))
+
+            best_scores, best_targets = scores.topk(2, dim=1, largest=True, sorted=True)
+
+            # update scores / potential targets
+            all_scores.append(best_scores.cpu())
+            all_targets.append((best_targets + i).cpu())
+
+        all_scores = torch.cat(all_scores, 1)
+        all_targets = torch.cat(all_targets, 1)
+
+        all_scores, best_targets = all_scores.topk(2, dim=1, largest=True, sorted=True)
+        all_targets = all_targets.gather(1, best_targets)
+
+    # contextual dissimilarity measure
+    elif dico_method.startswith('csls_knn_'):
+
+        knn = dico_method[len('csls_knn_'):]
+        assert knn.isdigit()
+        knn = int(knn)
+
+        # average distances to k nearest neighbors
+        average_dist1 = torch.from_numpy(get_nn_avg_dist(emb2, emb1, knn))
+        average_dist2 = torch.from_numpy(get_nn_avg_dist(emb1, emb2, knn))
+        average_dist1 = average_dist1.type_as(emb1)
+        average_dist2 = average_dist2.type_as(emb2)
+
+        # for every source word
+        for i in range(0, n_src, bs):
+
+            # compute target words scores
+            scores = emb2.mm(emb1[i:min(n_src, i + bs)].transpose(0, 1)).transpose(0, 1)
+            scores.mul_(2)
+            scores.sub_(average_dist1[i:min(n_src, i + bs)][:, None] + average_dist2[None, :])
+            best_scores, best_targets = scores.topk(2, dim=1, largest=True, sorted=True)
+
+            # update scores / potential targets
+            all_scores.append(best_scores.cpu())
+            all_targets.append(best_targets.cpu())
+
+        all_scores = torch.cat(all_scores, 0)
+        all_targets = torch.cat(all_targets, 0)
+
+    all_pairs = torch.cat([
+        torch.arange(0, all_targets.size(0)).long().unsqueeze(1),
+        all_targets[:, 0].unsqueeze(1)
+    ], 1)
+
+    # sanity check
+    assert all_scores.size() == all_pairs.size() == (n_src, 2)
+
+    # sort pairs by score confidence
+    diff = all_scores[:, 0] - all_scores[:, 1]
+    reordered = diff.sort(0, descending=True)[1]
+    all_scores = all_scores[reordered]
+    all_pairs = all_pairs[reordered]
+
+    # max dico words rank
+    if dico_max_rank > 0:
+        selected = all_pairs.max(1)[0] <= dico_max_rank
+        mask = selected.unsqueeze(1).expand_as(all_scores).clone()
+        all_scores = all_scores.masked_select(mask).view(-1, 2)
+        all_pairs = all_pairs.masked_select(mask).view(-1, 2)
+
+    # max dico size
+    if dico_max_size > 0:
+        all_scores = all_scores[:dico_max_size]
+        all_pairs = all_pairs[:dico_max_size]
+
+    # min dico size
+    diff = all_scores[:, 0] - all_scores[:, 1]
+    if dico_min_size > 0:
+        diff[:dico_min_size] = 1e9
+
+    # confidence threshold
+    if dico_threshold > 0:
+        mask = diff > dico_threshold
+#         logger.info("Selected %i / %i pairs above the confidence threshold." % (mask.sum(), diff.size(0)))
+        mask = mask.unsqueeze(1).expand_as(all_pairs).clone()
+        all_pairs = all_pairs.masked_select(mask).view(-1, 2)
+
+    return all_pairs
+
